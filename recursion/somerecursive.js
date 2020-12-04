e7d56dc610e04b8ebdbad2b2da4dfb29484ad505
GhanemYassine <46890231+GhanemYassine@users.noreply.github.com>
2020-12-04 13:58:13 +0100

    palindrome

---
+++ recursion/somerecursive.js
@@ -0,0 +1,9 @@
+function somerecursive(arr,cb){
+    if(!arr.length) return false;
+    if (!cb(arr[0])) return somerecursive(arr.slice(1),cb);
+    return true;
+}
+console.log(somerecursive([1, 2, 3, 4], val => val % 2 !== 0));
+console.log(somerecursive([4, 6, 8, 9], val => val % 2 !== 0));
+console.log(somerecursive([4, 6, 8], val => val % 2 !== 0));
+console.log(somerecursive([4, 6, 8], val => val > 10));

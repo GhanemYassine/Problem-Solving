19b860cd827dba8af26476f7d2241c8799c0a14d
GhanemYassine <46890231+GhanemYassine@users.noreply.github.com>
2020-12-04 14:00:02 +0100

    some recursive

--- recursion/somerecursive.js
+++ recursion/somerecursive.js
@@ -5,5 +5,5 @@ function somerecursive(arr,cb){
 }
 console.log(somerecursive([1, 2, 3, 4], val => val % 2 !== 0));
 console.log(somerecursive([4, 6, 8, 9], val => val % 2 !== 0));
-console.log(somerecursive([4, 6, 8], val => val % 2 !== 0));
+console.log(somerecursive([4, 6, 8, 10], val => val % 2 !== 0));
 console.log(somerecursive([4, 6, 8], val => val > 10));

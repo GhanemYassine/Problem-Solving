1045ca576f0f2f986c1ed48f8db80c11bd8ae86f
GhanemYassine <46890231+GhanemYassine@users.noreply.github.com>
2020-12-05 21:09:48 +0100

    nested even sum

---
+++ recursion/nestedEvenSum.js
@@ -0,0 +1,18 @@
+function nestedEvenSum (obj, sum=0) {
+    for (var key in obj) {
+        if (typeof obj[key] === 'object'){
+            sum += nestedEvenSum(obj[key]);
+        } else if (typeof obj[key] === 'number' && obj[key] % 2 === 0){
+            sum += obj[key];
+        }
+    }
+    return sum;
+}
+const obj = {
+    a: 2,
+    b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+    c: { c: { c: 2 }, cc: 'ball', ccc: 5 },
+    d: 1,
+    e: { e: { e: 2 }, ee: 'car' }
+  };
+console.log(nestedEvenSum(obj));

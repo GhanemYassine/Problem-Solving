b46de8c51941da2a0d2bc52eddb5af702716e165
GhanemYassine <mkacher1@live.com>
2022-02-21 19:59:25 +0100

    steps

---
+++ Random Problems/Steps.js
@@ -0,0 +1,15 @@
+function Steps(n){
+    for(let row=0; row<n; row++){
+        let stair='';
+        for(let col=0; col<n; col++){
+            if(col<=row){
+                stair+="#";
+            }else{
+                stair+=' ';
+            }
+        }
+        console.log(stair);
+    }
+}
+
+console.log(Steps(10));

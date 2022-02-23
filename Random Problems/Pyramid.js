31fa28a945c24cc385faf932515f57f7ea453fa1
GhanemYassine <mkacher1@live.com>
2022-02-23 23:57:13 +0100

    pyramid

--- Random Problems/Pyramid.js
+++ Random Problems/Pyramid.js
@@ -1,14 +1,15 @@
 //Sol 1
-function Steps(n){
+function Pyramid(n){
     for(let row=0; row<n; row++){
         let level='';
         for(let col=0; col<((n*2)-1); col++){
-            if(col<=row){
-                level+="#";
-            }else{
+            if(col<=(Math.floor(n*2)-1)/2){
                 level+=' ';
+            }else{
+                level+='#';
             }
         }
         console.log(level);
     }
 }
+console.log()

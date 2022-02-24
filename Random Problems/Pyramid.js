f3fd4f60b27438ddf5ee4dfc82b9d4e5c2ef002a
GhanemYassine <mkacher1@live.com>
2022-02-24 01:02:21 +0100

    Pyramid

--- Random Problems/Pyramid.js
+++ Random Problems/Pyramid.js
@@ -1,15 +1,16 @@
 //Sol 1
 function Pyramid(n){
+    const midPoint = Math.floor((2*n-1)/2);
     for(let row=0; row<n; row++){
         let level='';
-        for(let col=0; col<((n*2)-1); col++){
-            if(col<=(Math.floor(n*2)-1)/2){
-                level+=' ';
-            }else{
+        for(let col=0; col<2*n-1; col++){
+            if(midPoint - row <= col && midPoint + row >= col){
                 level+='#';
+            }else {
+                level+=' ';
             }
         }
         console.log(level);
     }
 }
-console.log()
+console.log(Pyramid(5));
